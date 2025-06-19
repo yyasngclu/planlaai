cc819808e549c87e3fec3ed446b30bfb08f4ad49
yyasngclu <yasn1650@gmail.com>
2025-06-19 07:51:01 +0300

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -24,7 +24,7 @@ function Header() {
           className="text-black-800 italic text-sm"
           style={{ marginLeft: "20px", marginTop: "5px" }}
         >
-          finans asistanı
+          finans asistanınız..
         </span>
       </div>
 
